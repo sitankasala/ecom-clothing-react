ea1ada66d4fde0b6ea1ab3189776a13309631c33
Sid <tankasala.siddhartha@gmail.com>
2023-05-20 11:21:07 +0530

    git commit -m "adding checkout page and total"

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-// import {render, screen} from '@testing-library/react';
-// import App from './App';
-//
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import {render, screen} from '@testing-library/react';
+import App from './App';
+import {MemoryRouter} from "react-router-dom";
+import {UserProvider} from "./contexts/users.context.components";
+import {ProductProvider} from "./contexts/products.context";
+import {CartProvider} from "./contexts/cart.context";
+
+test('renders learn react link', () => {
+    render(<MemoryRouter initialEntries={["/"]}>
+        <UserProvider>
+            <ProductProvider>
+                <CartProvider><App/>
+                </CartProvider>
+            </ProductProvider>
+        </UserProvider>
+    </MemoryRouter>);
+});
